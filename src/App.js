61a19ccc14f74abfaa503eb210e593156900b4c8
Jordi-GV-Ikea <jordi.gonzalez.verges@ingka.com>
2022-03-09 12:00:29 +0100

    setup react ga

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
+import React,{ useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactGA from 'react-ga';
 
 function App() {
+  const gaTrackingId = process.env.REACT_APP_GA_TRACKING_CODE;
+  console.log('Tracking id: ',gaTrackingId)
+  const setGA = () => {
+    ReactGA.initialize(gaTrackingId);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  };
+  useEffect (() => {
+    setGA();
+  }, [])
+
+
+
+const handleClick = () => {
+  console.log('HandleClick was clicked');
+  ReactGA.event({
+    action: 'click',
+    category: 'button'
+  })
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h4>
           Google Analytics using reac-ga
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+        </h4>
+        <img src={logo} className="App-logo" alt="logo" />
+        <a className="App-link"
+          href="https://wonderful-mestorf-270cbb.netlify.app/"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          rel="noopener noreferrer">https://wonderful-mestorf-270cbb.netlify.app/</a>
       </header>
+        <button onClick={handleClick}>click</button>
     </div>
   );
 }
