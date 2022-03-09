4f15da43807eea748454f6886ad206f1b5aef7eb
Jordi-GV-Ikea <jordi.gonzalez.verges@ingka.com>
2022-03-09 17:59:18 +0100

    three buttons

--- src/App.js
+++ src/App.js
@@ -12,11 +12,21 @@ function App() {
 
 
 const handleClick = (number) => {
-  console.log('HandleClick was clicked');
+  const actionSelected = `click ${number}`;
+  const categorySelected = `category ${number}`;
+    console.log('HandleClick was clicked');
   ReactGA.event({
-    action: 'click'+number,
-    category: 'button'+number,
+    action: actionSelected,
+    category: `category ${number}`,
   })
+}
+const handleClickSpecial = () => {
+  ReactGA.event({
+    action: 'custom',
+    category: 'myCategory',
+    label: 'new value',
+  })
+
 }
   return (
     <div className="App">
@@ -29,9 +39,13 @@ const handleClick = (number) => {
           target="_blank"
           rel="noopener noreferrer">https://zen-hamilton-5670d5.netlify.app/</a>
 }      </header>
-      <p>deploy: 17:30</p>
-        <button onClick={handleClick(1)}>click 1</button>
-        <button onClick={handleClick(2)}>click 2</button>
+      <p>deploy: 8</p>
+      <p>Action 1 // Category 1</p>
+        <button onClick={() => handleClick(1)}>click 1</button>
+        <p>Action 2 / Category 2</p>
+        <button onClick={() => handleClick(2)}>click 2</button>
+        <p>Action custom/ Category myCategory / label: new value</p>
+        <button onClick={() => handleClickSpecial()}>with label</button>
         <p>ID flujo: 3335026835</p>
         <p>ID medicion: G-TV6XY5N8M6</p>
     </div>
