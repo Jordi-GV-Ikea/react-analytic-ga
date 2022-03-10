c77947b9274084335d3e391c57860b81e6e61bb6
Jordi-GV-Ikea <jordi.gonzalez.verges@ingka.com>
2022-03-10 12:54:11 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -31,23 +31,26 @@ const handleClickSpecial = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h4>
-          Google Analytics using reac-ga
-        </h4>
+        {/*  */}
+        <h2>
+          Google Analytics using react-ga
+        </h2>
+     </header>
+        <button onClick={() => handleClick(1)}>action:click 1; category:category 1</button>
+        <button onClick={() => handleClick(2)}>action:click 2; category:category 2</button>
+        <button onClick={() => handleClickSpecial()}>action: 'custom',
+    category: 'myCategory',
+    label: 'new value',</button>
+        <footer>
+        <span>Repository: </span>
 {        <a className="App-link"
-          href="https://zen-hamilton-5670d5.netlify.app/"
+          href="https://github.com/Jordi-GV-Ikea/react-analytic-ga.git"
           target="_blank"
-          rel="noopener noreferrer">https://zen-hamilton-5670d5.netlify.app/</a>
-}      </header>
-      <p>deploy: 8</p>
-      <p>Action 1 // Category 1</p>
-        <button onClick={() => handleClick(1)}>click 1</button>
-        <p>Action 2 / Category 2</p>
-        <button onClick={() => handleClick(2)}>click 2</button>
-        <p>Action custom/ Category myCategory / label: new value</p>
-        <button onClick={() => handleClickSpecial()}>with label</button>
-        <p>ID flujo: 3335026835</p>
-        <p>ID medicion: G-TV6XY5N8M6</p>
+          rel="noopener noreferrer">https://github.com/Jordi-GV-Ikea/react-analytic-ga.git</a>}
+          <p>ID flujo: 3335026835</p>
+          <p>ID medicion: G-TV6XY5N8M6</p>
+          <p>deploy: 1</p>
+      </footer>
     </div>
   );
 }
