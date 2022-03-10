c1d7180ce49b236307092c21b5866217d6df5449
Jordi-GV-Ikea <jordi.gonzalez.verges@ingka.com>
2022-03-10 13:08:30 +0100

    senEvent

--- src/App.js
+++ src/App.js
@@ -12,35 +12,49 @@ function App() {
 
 
 const handleClick = (number) => {
-  const actionSelected = `click ${number}`;
-  const categorySelected = `category ${number}`;
-    console.log('HandleClick was clicked');
   ReactGA.event({
-    action: actionSelected,
+    action: `click ${number}`,
     category: `category ${number}`,
   })
 }
 const handleClickSpecial = () => {
   ReactGA.event({
-    action: 'custom',
-    category: 'myCategory',
-    label: 'new value',
+    action: 'action with label',
+    category: 'category with label',
+    label: 'my label',
   })
 
+}
+const sendMultiple = ()=> {
+  ReactGA.send({
+    hitType: 'event',
+    eventAction: 'action multiple',
+    eventCategory: 'category multiple',
+    eventLabel: 'label multiple',
+    custom: 'custom 1',
+    custom2: 'custom 2',
+    custom3: 'custom 3',
+  })
 }
   return (
     <div className="App">
       <header className="App-header">
         {/*  */}
         <h2>
-          Google Analytics using react-ga
+          Google Analytics 
         </h2>
      </header>
         <button onClick={() => handleClick(1)}>action:click 1; category:category 1</button>
         <button onClick={() => handleClick(2)}>action:click 2; category:category 2</button>
-        <button onClick={() => handleClickSpecial()}>action: 'custom',
-    category: 'myCategory',
-    label: 'new value',</button>
+        <button onClick={() => handleClickSpecial()}>  action: 'action with label',
+    category: 'category with label',
+    label: 'my label',</button>
+    <button onClick={() => sendMultiple()}>action: 'action multiple',
+    category: 'category multiple',
+    label: 'label multiple',
+    custom: 'custom 1',
+    custom2: 'custom 2',
+    custom3: 'custom 3',</button>
         <footer>
         <span>Repository: </span>
 {        <a className="App-link"
@@ -50,6 +64,7 @@ const handleClickSpecial = () => {
           <p>ID flujo: 3335026835</p>
           <p>ID medicion: G-TV6XY5N8M6</p>
           <p>deploy: 1</p>
+          <h2>react ga</h2>
       </footer>
     </div>
   );
