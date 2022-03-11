f102c2664e7ccbd83fea1bcf01e0ee0b72fd6771
Jordi-GV-Ikea <jordi.gonzalez.verges@ingka.com>
2022-03-11 11:27:21 +0100

    trackindID

--- src/App.js
+++ src/App.js
@@ -77,6 +77,7 @@ function App() {
             https://github.com/Jordi-GV-Ikea/react-analytic-ga.git
           </a>
         }
+        <p>trackingID: G-H0E6Z6MXW0</p>
       </footer>
     </div>
   );
