2c1ebde09411589bb1a992c1e1f4466a9247fc1b
Jordi-GV-Ikea <jordi.gonzalez.verges@ingka.com>
2022-03-11 08:36:22 +0100

    title

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
       <header className="App-header">
         {/*  */}
         <h2>gtag snippet</h2>
-        <p className='subtitle'>Global site tag (gtag.js) - Google Tag Manager</p>
+        <p className='subtitle'>Google Analytics & Google Tag Manager</p>
       </header>
 
       <div className="main>">
